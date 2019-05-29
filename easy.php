8411155df920fd18759110f7a03def153c1ad610
Mr. Zhang <c919342864@yahoo.com>
2019-05-29 09:44:36 +0800

    test

---
+++ easy.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     *  Author: 张吉凯 <919342864@qq.com>
+     *  Github: CrazyCodes <https://github.com/CrazyCodes>
+     *  Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+     */
+    include_once "./vendor/autoload.php";
+    
+    1111
